6e1968cf0b218afb0582b0a94b52001ab8fcffd1
virenribadiya <vribadiya278@rku.ac.in>
2023-02-11 22:13:26 +0530

    JSX

---
+++ src/ab.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Abcd() {
+    return <div><h1>Hello World.</h1><p>How are you?</p></div>
+}
+
+export default Abcd;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Abcd from './ab'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Abcd></Abcd>
+    
+  </React.StrictMode>,
+)
