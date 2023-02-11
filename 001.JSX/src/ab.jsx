0a552098180b44fa2796d0d13141274464e3f0b2
virenribadiya <vribadiya278@rku.ac.in>
2023-02-11 22:27:32 +0530

    jsx updated

--- 001.JSX/src/ab.jsx
+++ 001.JSX/src/ab.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Abcd() {
-    return <div><h1>Hello World.</h1><p>How are you?</p></div>
+    return (
+    <div>
+        <h1>Hello World.</h1>
+        <p>How are you?</p>
+        <h1>My Favourite Food:</h1>
+        <ul>
+            <li>Apple</li>
+            <li>Guava</li>
+            <li>Watermalon</li>
+        </ul>
+    </div>);
 }
 
 export default Abcd;
