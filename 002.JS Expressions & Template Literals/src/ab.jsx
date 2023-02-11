2904d4953f29521dd704d66591be502a6736855f
virenribadiya <vribadiya278@rku.ac.in>
2023-02-11 23:05:56 +0530

    copyright added

--- 002.JS Expressions & Template Literals/src/ab.jsx
+++ 002.JS Expressions & Template Literals/src/ab.jsx
@@ -3,6 +3,9 @@ import React from "react";
 const fname = "Viren";
 const lname = "Ribadiya";
 const num = 7;
+const currentDate = new Date();
+const year =currentDate.getFullYear();
+
 
 function Abcd() {
     return (
@@ -10,6 +13,7 @@ function Abcd() {
         <h1>Hello {fname + " " + lname}!</h1>
         {/* <h1>Hello {fname} {lname}!</h1> */}
         <p>Your lucky number is {num*Math.floor(Math.random()*10)}.</p>
+        <p>Copyright {year}</p>
     </div>
     );
 }
