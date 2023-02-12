c549a344b7429d918740a95ce4dec378187f3ec5
virenribadiya <vribadiya278@rku.ac.in>
2023-02-12 15:34:22 +0530

    components created

---
+++ 004.Components/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./components/Heading";
+import List from "./components/List";
+
+function App() {
+    return (
+    <div>
+        <Heading />
+        <List />  
+        <List />
+    </div>);
+}
+
+export default App;
---
+++ 004.Components/src/components/List.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function List() {
+    return (
+        <div>
+            <ul>
+                <li>Bacon</li>
+                <li>Jamon</li>
+                <li>Noodles</li>
+                <li>Soup</li>
+            </ul>
+        </div>
+    );
+}
+
+export default List;
